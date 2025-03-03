4eef188a3177845a1d8ea018834e2c2789c90c3d
VarshiyaTY <varshiyaty008@gmail.com>
2025-03-03 15:46:58 +0530

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,7 +1,7 @@
 public class Circle{
   public static void main(String args[]){
     int r=5;
-    int area=3.14*r*r;
+    double area=3.14*r*r;
     System.out.println("Area of Circle:"+area);
   }
 }
